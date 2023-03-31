3507e91c3c3cf6a165231f8ca04c7c91e3aa159b
Chris Davison <chris_davison@hotmail.com>
2023-03-31 16:12:32 +0100

    Mostly working Prusa system

--- smart3mf.py
+++ smart3mf.py
@@ -14,8 +14,8 @@ import xml.etree.ElementTree as ET
 #   post processing scripts, one of which can be a 'Filament change at layer'. So, use one such 3MF file as the base.
 
 #openscadexec = '"C:\Program Files\OpenSCAD\openscad.com"'	# Works fine except when wanting the -D param!
-openscadexec = "openscad.bat"
-openscadexec2 = "openscad2.bat"								# Used for when we also have a -D param
+openscadexec = "C:\git\Smart3mf\openscad.bat"
+openscadexec2 = "C:\git\Smart3mf\openscad2.bat"								# Used for when we also have a -D param
 
 hardcoded_modelpath = "3D/3dmodel.model"
 hardcoded_relsfile = '_rels/.rels'
@@ -61,23 +61,66 @@ def ExtractObject(file, newid, newname):
 
 	return resources[0]
 	
-def AddMetaDataGroup(object, addmodeltag):
-	mdgtag = addmodeltag.find('metadatagroup')		# This is the one from the config xml file.
+def AddCuraMetaData(object, addmodeltag):
+	mdgtag = addmodeltag.find('cura')		# This is the one from the config xml file.
 
 	dest = object.find('{http://schemas.microsoft.com/3dmanufacturing/core/2015/02}'+'metadatagroup')
 
-	print(object.tag, object.attrib)
+	#print(object.tag, object.attrib)
 
-	for child in object:
-		print(child.tag)
+	#for child in object:
+	#	print(child.tag)
 
-	if dest is not None:
-		print(mdgtag, dest)
+	#if dest is not None:
+	#	print(mdgtag, dest)
 	
 	if mdgtag:
 		object.insert(0, mdgtag)
 
-def WriteCombinedFile(steps, file, newobjects):
+def CombineModelData(namespace, resources, newobjects):
+	baseobj = resources.find('{'+namespace+'}object')
+	basemesh = baseobj.find('{'+namespace+'}mesh')
+	baseverts = basemesh.find('{'+namespace+'}vertices')
+	basetriangles = basemesh.find('{'+namespace+'}triangles')
+
+	index = 0
+	trianglecounts = []
+	curvertcount = len(baseverts)
+
+	trianglecounts.append(len(basetriangles))
+	
+	for newobj in newobjects:
+		meshdata = newobj.find('{'+namespace+'}mesh')
+		verts = meshdata.find('{'+namespace+'}vertices')
+		triangles = meshdata.find('{'+namespace+'}triangles')
+
+		trianglecounts.append(len(triangles))
+
+		for vert in verts:
+			baseverts.insert(len(baseverts), vert)
+
+		for triangle in triangles:
+			triangle.attrib['v1'] = str(int(triangle.attrib['v1']) + curvertcount)
+			triangle.attrib['v2'] = str(int(triangle.attrib['v2']) + curvertcount)
+			triangle.attrib['v3'] = str(int(triangle.attrib['v3']) + curvertcount)
+			basetriangles.insert(len(basetriangles), triangle)
+
+		curvertcount += len(verts)
+
+		print(trianglecounts)
+
+		return trianglecounts
+
+def AdjustStringMatrix(inputstring):
+	# This removes XYZ translation
+	print("transform", inputstring)
+	arr = [str(x) for x in inputstring.split(' ')]
+	arr[12] = "0.0"
+	arr[13] = "0.0"
+	arr[14] = "0.0"
+	return ''.join(arr)
+
+def WriteCombinedFile(steps, file, newobjects, mode):
 	basetag = steps.find('base')
 	my_namespaces = ReadNamespaces(file)
 	
@@ -88,13 +131,17 @@ def WriteCombinedFile(steps, file, newobjects):
 	base.parse(file)
 	modelroot = base.getroot()
 	resources = modelroot.find('{'+my_namespaces['']+'}'+'resources') # Passing in my_namespaces as the second param doesn't help
+
+	trianglecounts = []
 	
 	# Add any meta data
-	AddMetaDataGroup(resources[0], basetag)
+	if mode == "Cura":
+		AddCuraMetaData(resources[0], basetag)
 
-	for newobj in newobjects:
-		idx = len(resources)
-		resources.insert(idx, newobj)		# Insert at the end
+		for newobj in newobjects:
+			resources.insert(len(resources), newobj)		# Insert at the end
+	elif mode == "Prusa":
+		trianglecounts = CombineModelData(my_namespaces[''], resources, newobjects)
 		
 	# Update the <build> tag contents at the end of the 3dmodel.model file. 
 	
@@ -102,7 +149,12 @@ def WriteCombinedFile(steps, file, newobjects):
 	
 	for extobj in build:
 		#print('1=====>', extobj.get('objectid'), extobj.get('{'+'http://schemas.microsoft.com/3dmanufacturing/production/2015/06'+'}'+'UUID'))
-		extobj.set("transform", basetag.find('transform').text)
+		trans = basetag.find('transform').text
+
+		if mode == "Prusa":
+			trans = AdjustStringMatrix(trans)
+		
+		extobj.set("transform", trans)
 
 	addmodels = steps.findall('model')
 	index = 0
@@ -114,13 +166,16 @@ def WriteCombinedFile(steps, file, newobjects):
 		newnode.set("transform", trans)
 		#print('2=====>', newobj.get('id'), newobj.get('{'+'http://schemas.microsoft.com/3dmanufacturing/production/2015/06'+'}'+'UUID'))
 		
-		build.insert(len(build), newnode)		# Insert at the end
+		if mode == "Cura":
+			build.insert(len(build), newnode)		# Insert at the end
 		
 		index += 1
 
 	# Done. Write it out.
 	
 	base.write(file, encoding='UTF-8', xml_declaration=True)
+
+	return trianglecounts
 
 def ExportStep(key, value, stepfileout, input_scad):
 	cmd = openscadexec2 + " " + key + " " + value + " -o " + stepfileout + " " + input_scad
@@ -152,7 +207,7 @@ def ProcessStep(step, folder, models, extractall):
 		with zipfile.ZipFile(stepfileout, 'r') as myzip:
 			with myzip.open(hardcoded_modelpath) as myfile:
 				newobj = ExtractObject(myfile, len(models)+2, step.find('name').text)
-				AddMetaDataGroup(newobj, step)
+				AddCuraMetaData(newobj, step)
 				models.append(newobj)
 				
 		# Delete the 3mf file as we have got what we need.
@@ -235,16 +290,65 @@ def ZipFolder(targetfile, sourcefolder):
 	
 	print('Output:', targetfile)
 
+def CreateSlic3rModelConfig(configroot, tempfolder, trianglecounts):
+	# Check the base model first.
+	
+	prusabase = configroot.find('export').find('base').find('prusa')
+	
+	if not prusabase:
+		return
+	
+	root = ET.Element('config')
+	obj = ET.SubElement(root, 'object')
+	obj.set('id', '1')
+	obj.set('instances_count', '1')
+	rootmeta = ET.SubElement(obj, 'metadata')
+	rootmeta.set('type', 'object')
+	rootmeta.set('key', 'name')
+	rootmeta.set('value', configroot.find('export').attrib['file'])
+
+	index = 0
+	tricount = trianglecounts[index]
+	
+	v1 = ET.SubElement(obj, 'volume')
+	v1.set('firstid', '0')
+	v1.set('lastid', str(tricount-1))									# '219'
+	
+	for el in prusabase:
+		v1.append(el)
+	
+	# Now do each of the sub model.
+	
+	model = configroot.find('export').findall('model')
+	
+	for m in model:
+		prusamodel = m.find('prusa')
+	
+		v2 = ET.SubElement(obj, 'volume')
+		v2.set('firstid', str(tricount))								# '220'
+		index += 1
+		tricount += trianglecounts[index]
+		v2.set('lastid', str(tricount-1))								# '323'
+		
+		for el in prusamodel:
+			v2.append(el)
+	
+	tree = ET.ElementTree(root)
+	tree.write(tempfolder+'/Metadata/Slic3r_PE_model.config', xml_declaration=True, encoding='utf-8')
+
 ######
 # MAIN
 ######
 
 parser = argparse.ArgumentParser(description='3MF file manipulator')
 parser.add_argument('settingsxml', help='Your settings xml file')
+parser.add_argument('--mode', choices=['Cura', 'Prusa'], help='Use Cura or Prusa focused 3mf output')
 args = parser.parse_args()
 
 # Run the OpenSCAD executable with the version param. Should spit out a single line like 'OpenSCAD version 2021.01' to
-# check that the path is correct and abort straight away if not found. 
+# check that the path is correct and abort straight away if not found.
+
+print("Mode", args.mode)
 
 ret = os.system(openscadexec + " -v")
 if ret != 0:
@@ -269,7 +373,7 @@ with tempfile.TemporaryDirectory() as tempfolder:
 		quit()
 
 	models = ProcessSteps(steps, tempfolder)
-	WriteCombinedFile(steps, tempfolder+"/"+hardcoded_modelpath, models)
+	trianglecounts = WriteCombinedFile(steps, tempfolder+"/"+hardcoded_modelpath, models, args.mode)
 		
 	# Thumbnail (Optional). If present, two options, either generate one using SCAD, or
 	# just use one provided as is.
@@ -286,6 +390,9 @@ with tempfile.TemporaryDirectory() as tempfolder:
 			CopyThumbnail(image_tag, tempfolder+'/Metadata/')
 		
 		UpdateRelsFile(tempfolder+'/'+hardcoded_relsfile)
+
+	if args.mode == 'Prusa':
+		CreateSlic3rModelConfig(configroot, tempfolder, trianglecounts)
 
 	# Everything should be ready, so zip up the temp folder.
 	ZipFolder(output_3mf, tempfolder)
